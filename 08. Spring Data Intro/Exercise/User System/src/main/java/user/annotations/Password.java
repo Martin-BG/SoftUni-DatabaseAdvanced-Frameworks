ed4821f1d7b11a752cc0e8e808e4333c11c1fa7e
MartinBG <MartinBG@abv.bg>
2018-04-02 23:00:31 +0300

    Password annotations

---
+++ 08. Spring Data Intro/Exercise/User System/src/main/java/user/annotations/Password.java
@@ -0,0 +1,35 @@
+package user.annotations;
+
+import org.springframework.stereotype.Component;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+
+    String message() default "Invalid password format";
+
+    int minLength() default 6;
+
+    int maxLength() default 30;
+
+    boolean containsDigit() default false;
+
+    boolean containsLowerCase() default false;
+
+    boolean containsUpperCase() default false;
+
+    boolean containsSpecialSymbols() default false;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ 08. Spring Data Intro/Exercise/User System/src/main/java/user/annotations/PasswordValidator.java
@@ -0,0 +1,90 @@
+package user.annotations;
+
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class PasswordValidator implements ConstraintValidator<Password, CharSequence> {
+
+    private int minLength;
+    private int maxLength;
+    private boolean hasUpper;
+    private boolean hasLower;
+    private boolean hasDigit;
+    private boolean hasSpecialSymbol;
+
+    @Override
+    public void initialize(Password password) {
+        this.minLength = password.minLength();
+        this.maxLength = password.maxLength();
+        this.hasUpper = password.containsUpperCase();
+        this.hasLower = password.containsLowerCase();
+        this.hasDigit = password.containsDigit();
+        this.hasSpecialSymbol = password.containsSpecialSymbols();
+    }
+
+    @Override
+    public boolean isValid(CharSequence charSequence, ConstraintValidatorContext constraintValidatorContext) {
+        if (charSequence == null || charSequence.length() < this.minLength || charSequence.length() > this.maxLength) {
+            return false;
+        }
+
+        boolean upper = false;
+        boolean lower = false;
+        boolean digit = false;
+        boolean specialSymbol = false;
+
+        if (this.hasUpper) {
+            for (int i = 0; i < charSequence.length(); i++) {
+                if (Character.isUpperCase(charSequence.charAt(i))) {
+                    upper = true;
+                    break;
+                }
+            }
+        } else {
+            upper = true;
+        }
+
+        if (this.hasLower) {
+            for (int i = 0; i < charSequence.length(); i++) {
+                if (Character.isLowerCase(charSequence.charAt(i))) {
+                    lower = true;
+                    break;
+                }
+            }
+        } else {
+            lower = true;
+        }
+
+        if (this.hasDigit) {
+            for (int i = 0; i < charSequence.length(); i++) {
+                if (Character.isDigit(charSequence.charAt(i))) {
+                    digit = true;
+                    break;
+                }
+            }
+        } else {
+            digit = true;
+        }
+
+        if (this.hasSpecialSymbol) {
+            List<Character> specialSymbols = new ArrayList<>();
+            Collections.addAll(specialSymbols, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '<', '>', '?');
+            for (int i = 0; i < charSequence.length(); i++) {
+                if (specialSymbols.contains(charSequence.charAt(i))) {
+                    specialSymbol = true;
+                    break;
+                }
+            }
+        } else {
+            specialSymbol = true;
+        }
+
+        return upper && lower && digit && specialSymbol;
+    }
+}
