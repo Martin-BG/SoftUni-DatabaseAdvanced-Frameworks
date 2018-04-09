bc5c0d5e212d59f5b8ce8473028a8d6a7becbf12
MartinBG <MartinBG@abv.bg>
2018-04-09 15:06:58 +0300

    Added remaining commands

--- 10. Spring Data Auto Mapping Objects/exercise/game_store/src/main/java/game_store/constants/CommandConstants.java
+++ 10. Spring Data Auto Mapping Objects/exercise/game_store/src/main/java/game_store/constants/CommandConstants.java
@@ -8,16 +8,23 @@ public final class CommandConstants {
     public static final String INPUT_PARAMS_SEPARATOR = "=";
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("d-M-yyyy");
 
+    public static final String END_COMMAND = "End";
+
     public static final String REGISTER_USER = "RegisterUser";
+    public static final String LOGIN_USER = "LoginUser";
+    public static final String LOGOUT_USER = "LogoutUser";
+
     public static final String ADD_GAME = "AddGame";
     public static final String EDIT_GAME = "EditGame";
     public static final String DELETE_GAME = "DeleteGame";
-    public static final String LOGIN_USER = "LoginUser";
-    public static final String LOGOUT_USER = "LogoutUser";
-    public static final String END_COMMAND = "End";
+
     public static final String ALL_GAME = "AllGame";
     public static final String DETAIL_GAME = "DetailGame";
     public static final String OWNED_GAME = "OwnedGame";
+
+    public static final String ADD_ITEM = "AddItem";
+    public static final String REMOVE_ITEM = "RemoveItem";
+    public static final String BUY_ITEM = "BuyItem";
 
     private CommandConstants() {
     }
