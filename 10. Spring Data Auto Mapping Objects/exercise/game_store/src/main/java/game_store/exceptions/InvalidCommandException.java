c1a1eee53de9ccae80fdcabafc719ab991782509
MartinBG <MartinBG@abv.bg>
2018-04-09 15:35:54 +0300

    Added serialVersionUID

--- 10. Spring Data Auto Mapping Objects/exercise/game_store/src/main/java/game_store/exceptions/InvalidCommandException.java
+++ 10. Spring Data Auto Mapping Objects/exercise/game_store/src/main/java/game_store/exceptions/InvalidCommandException.java
@@ -2,6 +2,8 @@ package game_store.exceptions;
 
 public class InvalidCommandException extends RuntimeException {
 
+    private static final long serialVersionUID = 5876844115664006274L;
+
     public InvalidCommandException(final String message) {
         super(message);
     }
