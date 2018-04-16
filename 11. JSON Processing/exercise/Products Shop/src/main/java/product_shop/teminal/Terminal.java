1e159806b96180ec66890aa1bde15b24464936d0
MartinBG <MartinBG@abv.bg>
2018-04-16 10:12:50 +0300

    File I/O refactoring

--- 11. JSON Processing/exercise/Products Shop/src/main/java/product_shop/teminal/Terminal.java
+++ 11. JSON Processing/exercise/Products Shop/src/main/java/product_shop/teminal/Terminal.java
@@ -18,6 +18,13 @@ import java.math.BigDecimal;
 @Transactional
 public class Terminal implements CommandLineRunner {
 
+    private static final String RESOURCES_PATH = System.getProperty("user.dir") + "/src/main/resources/";
+    private static final String SEED_USERS_JSON = RESOURCES_PATH + "seed/users.json";
+    private static final String SEED_CATEGORIES_JSON = RESOURCES_PATH + "seed/categories.json";
+    private static final String SEED_PRODUCTS_JSON = RESOURCES_PATH + "seed/products.json";
+    private static final String OUTPUT_PRODUCTS_IN_RANGE_JSON = RESOURCES_PATH + "output/products-in-range.json";
+    private static final String OUTPUT_USERS_SOLD_PRODUCTS_JSON = RESOURCES_PATH + "output/users-sold-products.json";
+
     private final JsonParser jsonParser;
     private final CategoryServiceImpl categoryService;
     private final ProductServiceImpl productService;
@@ -45,26 +52,26 @@ public class Terminal implements CommandLineRunner {
     }
 
     private void seedDatabase() {
-        UserDto[] usersDto = this.jsonParser.objectFromFile(UserDto[].class, "/seed/users.json");
+        UserDto[] usersDto = this.jsonParser.objectFromFile(UserDto[].class, SEED_USERS_JSON);
         this.userService.saveAll(usersDto);
 
-        CategoryDto[] categoriesDto = this.jsonParser.objectFromFile(CategoryDto[].class, "/seed/categories.json");
+        CategoryDto[] categoriesDto = this.jsonParser.objectFromFile(CategoryDto[].class, SEED_CATEGORIES_JSON);
         this.categoryService.saveAll(categoriesDto);
 
-        ProductDto[] productsDto = this.jsonParser.objectFromFile(ProductDto[].class, "/seed/products.json");
+        ProductDto[] productsDto = this.jsonParser.objectFromFile(ProductDto[].class, SEED_PRODUCTS_JSON);
         this.productService.saveAll(productsDto);
     }
 
     private void saveAvailableProductsInPriceRange(BigDecimal low, BigDecimal high) {
         this.jsonParser.objectToFile(
                 this.productService.getAvailableProductsInPriceRange(low, high),
-                "output/products-in-range.json");
+                OUTPUT_PRODUCTS_IN_RANGE_JSON);
 
     }
 
     private void saveSuccessfulSellers() {
         this.jsonParser.objectToFile(
                 this.userService.getSuccessfulSellers(),
-                "output/users-sold-products.json");
+                OUTPUT_USERS_SOLD_PRODUCTS_JSON);
     }
 }
--- 11. JSON Processing/exercise/Products Shop/src/main/java/product_shop/utils/TextFileUtils.java
+++ 11. JSON Processing/exercise/Products Shop/src/main/java/product_shop/utils/TextFileUtils.java
@@ -3,6 +3,7 @@ package product_shop.utils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StreamUtils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -14,30 +15,19 @@ public final class TextFileUtils {
     }
 
     public static String read(String path) {
-        try (final InputStream inputStream = TextFileUtils.class.getResourceAsStream(path)) {
+        try (final InputStream inputStream = new FileInputStream(path)) {
             return StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
-
-//        final StringBuilder sb = new StringBuilder();
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(
-//                ResourceUtils.getFile("classpath:" + path)),
-//                StandardCharsets.UTF_8))) {
-//            reader.lines().forEach(sb::append);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return sb.toString();
     }
 
-    public static void write(String json, String path) {
+    public static void write(String text, String path) {
         try {
             Files.write(
-                    ResourceUtils.getFile("classpath:" + path).toPath(),
-                    json.getBytes(StandardCharsets.UTF_8));
+                    ResourceUtils.getFile(path).toPath(),
+                    text.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
