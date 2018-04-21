9d68b0fab7c4f4afa160aca4d1d100ac1f47c603
MartinBG <MartinBG@abv.bg>
2018-04-21 18:45:35 +0300

    FIleIOImpl implementation

--- 14. Exam Preparations/Animal/src/main/java/app/retake/io/FIleIOImpl.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/io/FIleIOImpl.java
@@ -2,15 +2,34 @@ package app.retake.io;
 
 import app.retake.io.api.FileIO;
 
+import java.io.*;
+
 public class FIleIOImpl implements FileIO {
 
     @Override
     public String read(String file) {
-        return null;
+        StringBuilder fileContent = new StringBuilder();
+
+        try (InputStream inputStream = getClass().getResourceAsStream(file);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                fileContent.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileContent.toString();
     }
 
     @Override
     public void write(String fileContent, String file) {
-
+        try (OutputStream outputStream = new FileOutputStream(file);
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream))) {
+            bufferedWriter.write(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
