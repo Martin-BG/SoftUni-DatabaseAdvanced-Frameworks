a9d15e51e5c49ca37e1fc3e3d64c19be1731a21a
MartinBG <MartinBG@abv.bg>
2018-04-21 19:55:21 +0300

    ValidationUtil implementation

--- 14. Exam Preparations/Animal/src/main/java/app/retake/parser/ValidationUtil.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/parser/ValidationUtil.java
@@ -1,7 +1,16 @@
 package app.retake.parser;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 public final class ValidationUtil {
+
+    private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    private ValidationUtil() {
+    }
+
     public static <T> boolean isValid(T t) {
-        return false;
+        return t != null && validator.validate(t).isEmpty();
     }
 }
