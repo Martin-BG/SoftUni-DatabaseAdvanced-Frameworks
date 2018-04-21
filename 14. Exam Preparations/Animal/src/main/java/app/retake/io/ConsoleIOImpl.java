adf255bd4da2b177ec1957bb700575ba495f888a
MartinBG <MartinBG@abv.bg>
2018-04-21 18:40:28 +0300

    ConsoleIOImpl implementation

--- 14. Exam Preparations/Animal/src/main/java/app/retake/io/ConsoleIOImpl.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/io/ConsoleIOImpl.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Component;
 public class ConsoleIOImpl implements ConsoleIO {
     @Override
     public void write(String line) {
+        System.out.println(line);
     }
 }
