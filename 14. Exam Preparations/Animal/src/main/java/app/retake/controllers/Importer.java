317ee57e3391c2a4e415df97d2e50ac0305efb82
MartinBG <MartinBG@abv.bg>
2018-04-21 22:29:37 +0300

    refactoring

--- 14. Exam Preparations/Animal/src/main/java/app/retake/controllers/Importer.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/controllers/Importer.java
@@ -2,19 +2,38 @@ package app.retake.controllers;
 
 import app.retake.domain.dto.Importable;
 import app.retake.parser.ValidationUtil;
+import app.retake.parser.interfaces.Parser;
 import app.retake.services.api.Creatable;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
 
 @Controller
-class Importer {
+final class Importer {
 
-    <T extends Importable> String persist(@NotNull T[] dtos, @NotNull Creatable repository) {
+    @Validated
+    <T extends Importable> String importData(final @NotNull Parser parser,
+                                             final @NotNull T[] t,
+                                             final @NotNull String content,
+                                             final @NotNull Creatable service) {
+        try {
+            return this.persist(
+                    parser.read(t.getClass(), content),
+                    service);
+        } catch (IOException | JAXBException e) {
+            return e.getMessage();
+        }
+    }
+
+    private <T extends Importable> String persist(final @NotNull T[] dtos,
+                                                  final @NotNull Creatable service) {
         final StringBuilder sb = new StringBuilder();
         for (T dto : dtos) {
             if (ValidationUtil.isValid(dto)) {
-                repository.create(dto);
+                service.create(dto);
                 sb.append(dto.successMessage()).append(System.lineSeparator());
             } else {
                 sb.append(dto.errorMessage()).append(System.lineSeparator());
