d3eda96a3a473c08cf5adad7db0fc8e9f98f53bd
MartinBG <MartinBG@abv.bg>
2018-04-21 18:24:26 +0300

    ModelParser implementation

--- 14. Exam Preparations/Animal/src/main/java/app/retake/parser/ModelParserImpl.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/parser/ModelParserImpl.java
@@ -1,23 +1,27 @@
 package app.retake.parser;
 
 import app.retake.parser.interfaces.ModelParser;
+import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class ModelParserImpl implements ModelParser {
 
-    public ModelParserImpl() {
+    private final ModelMapper modelMapper;
 
+    public ModelParserImpl() {
+        this.modelMapper = new ModelMapper();
     }
 
     @Override
     public <S, D> D convert(S source, Class<D> destinationClass) {
-        return null;
+        return this.modelMapper.map(source, destinationClass);
     }
 
     @Override
     public <S, D> D convert(S source, Class<D> destinationClass, PropertyMap<S, D> propertyMap) {
-        return null;
+        this.modelMapper.addMappings(propertyMap);
+        return this.convert(source, destinationClass);
     }
 }
