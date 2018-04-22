8639e2182f6aca1a7fa231aa6d57a65919d8c942
MartinBG <MartinBG@abv.bg>
2018-04-22 07:57:12 +0300

    Added custom date serialize and deserialize classes

--- 14. Exam Preparations/Animal/src/main/java/app/retake/parser/JSONParser.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/parser/JSONParser.java
@@ -1,9 +1,13 @@
 package app.retake.parser;
 
 import app.retake.parser.interfaces.Parser;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component(value = "JSONParser")
 public class JSONParser implements Parser {
@@ -13,7 +17,9 @@ public class JSONParser implements Parser {
     public JSONParser() {
         this.gson = new GsonBuilder()
 //                .excludeFieldsWithoutExposeAnnotation()
-                .setDateFormat("dd-MM-yyyy")
+//                .setDateFormat("dd-MM-yyyy")
+                .registerTypeAdapter(Date.class, new DateDeserializer())
+                .registerTypeAdapter(Date.class, new DateSerializer())
                 .setPrettyPrinting()
                 .create();
     }
@@ -27,4 +33,44 @@ public class JSONParser implements Parser {
     public <T> String write(T object) {
         return this.gson.toJson(object);
     }
+
+    private static class DateSerializer implements JsonSerializer<Date> {
+        @Override
+        public JsonElement serialize(final Date date, final Type typeOfSrc, final JsonSerializationContext context) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+            return new JsonPrimitive(sdf.format(date));
+        }
+    }
+
+    private static class DateDeserializer implements JsonDeserializer<Date> {
+        @Override
+        public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            Date date = null;
+            try {
+                date = sdf.parse(json.getAsJsonPrimitive().getAsString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return date;
+        }
+    }
+
+    /*      With Lambda:
+
+            .registerTypeAdapter(Date.class, dateJsonSerializer)
+            .registerTypeAdapter(Date.class, dateJsonDeserializer)
+
+        JsonSerializer<Date> dateJsonSerializer =
+                (json, typeOfT, context) -> json == null ? null : new JsonPrimitive(new SimpleDateFormat("dd-MMM-yyyy").format(json));
+
+        JsonDeserializer<Date> dateJsonDeserializer =
+                (json, typeOfT, context) -> {
+                    try {
+                        return json == null ? null : new SimpleDateFormat("dd-MM-yyyy").parse(json.getAsJsonPrimitive().getAsString());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                };*/
 }
