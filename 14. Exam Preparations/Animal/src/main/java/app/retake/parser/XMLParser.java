67b3f7784bdcede778bce1e6da899326003e1336
MartinBG <MartinBG@abv.bg>
2018-04-22 05:53:44 +0300

    Added DateTimeFormatter adapter class

--- 14. Exam Preparations/Animal/src/main/java/app/retake/parser/XMLParser.java
+++ 14. Exam Preparations/Animal/src/main/java/app/retake/parser/XMLParser.java
@@ -7,18 +7,29 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component(value = "XMLParser")
 public class XMLParser implements Parser {
+
+    private final DateTimeAdapter dateTimeAdapter;
+
+    public XMLParser() {
+        this.dateTimeAdapter = new DateTimeAdapter();
+    }
+
 
     @Override
     @SuppressWarnings("unchecked")
     public <T> T read(Class<T> objectClass, String fileContent) throws JAXBException {
         final JAXBContext jaxbContext = JAXBContext.newInstance(objectClass);
         final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        unmarshaller.setAdapter(this.dateTimeAdapter);
         return (T) unmarshaller.unmarshal(new StringReader(fileContent));
     }
 
@@ -28,8 +39,24 @@ public class XMLParser implements Parser {
         final Marshaller marshaller = jaxbContext.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
         marshaller.setProperty(Marshaller.JAXB_ENCODING, StandardCharsets.UTF_8.displayName());
+        marshaller.setAdapter(this.dateTimeAdapter);
         final StringWriter writer = new StringWriter();
         marshaller.marshal(object, writer);
         return writer.toString();
+    }
+
+    @Component
+    public static class DateTimeAdapter extends XmlAdapter<String, Date> {
+        private final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+        @Override
+        public Date unmarshal(String v) throws Exception {
+            return this.format.parse(v);
+        }
+
+        @Override
+        public String marshal(Date v) {
+            return this.format.format(v);
+        }
     }
 }
